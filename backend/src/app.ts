2428ff4bcf7ece9bf3ee67e2972bd356c698b472
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2023-11-01 17:31:26 +0530

    cors added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,19 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+
 import { connect } from "./db";
 import todoRouter  from "./routes/todoRoutes"
 configDotenv();
+const cors = require("cors")
 const PORT = process.env.PORT || 5000;
 // connecting mongo DB
 connect();
 
 
+
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use("/api",todoRouter)
 
 app.listen(PORT, () => {
