3295e4cd2c7dbcc74efb4988a516e2aff29cdcb6
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2023-11-03 19:48:21 +0530

    deployment changes

--- backend/src/Index.ts
+++ backend/src/Index.ts
@@ -1,23 +1,20 @@
 import express from "express";
 import { configDotenv } from "dotenv";
-
 import { connect } from "./db";
-import todoRouter  from "./routes/todoRoutes"
+import todoRouter from "./routes/todoRoutes";
 configDotenv();
-const cors = require("cors")
+const cors = require("cors");
 const PORT = process.env.PORT || 5500;
 // connecting mongo DB
 connect();
 
-
-
 const app = express();
 app.use(express.json());
-app.use(cors())
-app.use("/api",todoRouter)
-app.get('/',(req,res)=>{
-  res.json({message:"Hello"})
-})
+app.use(cors());
+app.use("/api", todoRouter);
+app.get("/", (req, res) => {
+  res.json({ message: "Hello" });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
 });
